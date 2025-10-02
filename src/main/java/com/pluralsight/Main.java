d3e03eb109841fa5696bb0ce6c4a21b990b17dea
Jaylenh47-stack <jaylenh47@gmail.com>
2025-10-01 21:47:42 -0400

    Complete

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -8,37 +8,62 @@ public class Main {
     public static void main(String[] args) {
 
         //Prompt user for their address, Store it in String builder and append the information,display information
-        //Grover Smith
-        //Billing Address:
-        //123 Main Street
-        //Middleton, TX 75111
-        //Shipping Address:
-        //456 Big Sky Blvd
-        //Outer Rim, TX 75333
+//        Grover Smith
 
-        //Variables needed, remember to get from user
+//        Billing Address:
+//        123 Main Street
+//        Middleton, TX 75111
+//
+//        Shipping Address:
+//        456 Big Sky Blvd
+//        Outer Rim, TX 75333
+
+
         StringBuilder fullAddress = new StringBuilder();
-
         Scanner scanner = new Scanner(System.in);
 
-        String namePrompt = "Full name: ";
-        System.out.print(namePrompt);
+        //user Name
+        System.out.print("Full name: ");
         String name = scanner.nextLine();
-        fullAddress.append(name + "\n");
+        fullAddress.append(name + "\n\n");
 
-        String billingStreetPrompt = "Billing Street: ";
-        System.out.print(billingStreetPrompt);
+        fullAddress.append("Billing Address:\n");
+
+        System.out.print("Billing Street: ");
         String billingStreet = scanner.nextLine();
-        fullAddress.append(billingStreet);
+        fullAddress.append(billingStreet +"\n");
 
-        String billingCityPrompt = "Billing City: ";
-        String billingStatePrompt = "Billing State: ";
-        String billingZipPrompt = "Billing Zip: ";
+        System.out.print("Billing City: ");
+        String billingCity = scanner.nextLine();
+        fullAddress.append(billingCity +"," + " ");
 
-        String shippingStreetPrompt = "Shipping Street: ";
-        String shippingCityPrompt = "Shipping City: ";
-        String shippingStatePrompt = "Shipping State: ";
-        String shippingZipPrompt = "Shipping Zip";
+        System.out.print("Billing State: ");
+        String billingState = scanner.nextLine();
+        fullAddress.append(billingState + " ");
+
+        System.out.print("Billing Zip: ");
+        int billingZip = scanner.nextInt();
+        scanner.nextLine();
+        fullAddress.append(billingZip + "\n\n");
+
+        fullAddress.append("Shipping Address: \n" );
+
+        System.out.print("Shipping Street: ");
+        String shippingStreet = scanner.nextLine();
+        fullAddress.append(shippingStreet + "\n");
+
+        System.out.print("Shipping City: ");
+        String shippingCity = scanner.nextLine();
+        fullAddress.append(shippingCity + "," + " ");
+
+        System.out.print("Shipping State: ");
+        String shippingState = scanner.nextLine();
+        fullAddress.append(shippingState + " ");
+
+        System.out.print("Shipping Zip: ");
+        int shippingZip = scanner.nextInt();
+        scanner.nextLine();
+        fullAddress.append(shippingZip);
 
         String fullAddressString = fullAddress.toString();
         System.out.println(fullAddress);
