53ce184c355f5dc1888a0581e015f524838a4328
Jaylenh47-stack <jaylenh47@gmail.com>
2025-10-01 21:18:40 -0400

    basic structure added

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,47 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //Prompt user for their address, Store it in String builder and append the information,display information
+        //Grover Smith
+        //Billing Address:
+        //123 Main Street
+        //Middleton, TX 75111
+        //Shipping Address:
+        //456 Big Sky Blvd
+        //Outer Rim, TX 75333
+
+        //Variables needed, remember to get from user
+        StringBuilder fullAddress = new StringBuilder();
+
+        Scanner scanner = new Scanner(System.in);
+
+        String namePrompt = "Full name: ";
+        System.out.print(namePrompt);
+        String name = scanner.nextLine();
+        fullAddress.append(name + "\n");
+
+        String billingStreetPrompt = "Billing Street: ";
+        System.out.print(billingStreetPrompt);
+        String billingStreet = scanner.nextLine();
+        fullAddress.append(billingStreet);
+
+        String billingCityPrompt = "Billing City: ";
+        String billingStatePrompt = "Billing State: ";
+        String billingZipPrompt = "Billing Zip: ";
+
+        String shippingStreetPrompt = "Shipping Street: ";
+        String shippingCityPrompt = "Shipping City: ";
+        String shippingStatePrompt = "Shipping State: ";
+        String shippingZipPrompt = "Shipping Zip";
+
+        String fullAddressString = fullAddress.toString();
+        System.out.println(fullAddress);
+
     }
 }
